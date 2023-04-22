70f46c743ef125214bd40187ca7ec7b8b41a9a32
Aleksandar Atanasov <aleks930819@gmail.com>
2023-04-22 12:04:18 +0300

    added login and register view

--- login.php
+++ login.php
@@ -22,9 +22,8 @@ if (isset($_POST["submit_button"])) {
         </div>
 
         <div class="column">
-
             <form action="register.php" method="POST">
-                <h2>Sign Up</h2>
+                <h2>Sign In</h2>
                 <input type="text" name="first_name" placeholder="First Name" required>
                 <input type="text" name="last_name" placeholder="Last Name" required>
                 <input type="text" name="user_name" placeholder="Username" required>
@@ -32,8 +31,8 @@ if (isset($_POST["submit_button"])) {
                 <input type="email" name="confirm_email" placeholder="Confirm Email" required>
                 <input type="password" name="password" placeholder="Password" required>
                 <input type="password" name="confirm_password" placeholder="Confrim Password" required>
-                <a href="login.php">Already have an account? Sign in here.</a>
-                <button name="submit_button">Sign Up</button>
+                <a href="register.php">Don't have an account? Sign up here.</a>
+                <button name="submit_button">Sign In</button>
             </form>
         </div>
 </body>
