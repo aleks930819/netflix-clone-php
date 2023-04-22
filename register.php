c54399492b1a52cfe4dc3b48c83aa1b5682773b3
Aleksandar Atanasov <aleks930819@gmail.com>
2023-04-22 11:29:32 +0300

    inital commit

---
+++ register.php
@@ -0,0 +1,25 @@
+<?php
+
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Netflix</title>
+    <link rel="stylesheet" type="text/css" href="assets/styles/style.css" />
+</head>
+
+<body>
+    <form action="register.php" method="post">
+        <input type="text" name="username" placeholder="Username">
+        <input type="password" name="password" placeholder="Password">
+        <input type="submit" name="submit" value="Register">
+    </form>
+</body>
+
+</html>
